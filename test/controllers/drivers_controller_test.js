26c91b8bec1389f8a5d1d0b2890177239fdea348
Sooraj Dube <soorajdube.is@gmail.com>
2018-08-10 12:40:32 +0530

    delete test

--- test/controllers/drivers_controller_test.js
+++ test/controllers/drivers_controller_test.js
@@ -8,32 +8,50 @@ describe('Driver Controller', () => {
 
   it('Makes post request to api/create', (done) => {
     Driver.countDocuments()
-            .then(count => {
-              request(app)
-              .post('/api/drivers')
-              .send({ email: 'test@test.com' })
-              .end(() => {
-                  Driver.countDocuments()
-                        .then(newCount => {
-                          assert(count + 1 === newCount) 
-                          done(); 
-                        })
+      .then(count => {
+        request(app)
+          .post('/api/drivers')
+          .send({ email: 'test@test.com' })
+          .end(() => {
+            Driver.countDocuments()
+              .then(newCount => {
+                assert(count + 1 === newCount)
+                done();
               })
-            })
+          })
+      })
   })
 
   it('makes a put request to api/drivers to edit', done => {
-    const driver = new Driver({ email: 't@t.com'})
+    const driver = new Driver({ email: 't@t.com' })
     driver.save()
-          .then(() => {
-            request(app)
-                    .put(`/api/drivers/${driver._id}`)
-                    .send({ driving: true})
-                    .end((err, res) => {
-                      assert(res.body.driving === true)
-                      done();
-                    })
+      .then(() => {
+        request(app)
+          .put(`/api/drivers/${driver._id}`)
+          .send({ driving: true })
+          .end((err, res) => {
+            assert(res.body.driving === true)
+            done();
           })
+      })
   })
-  
+
+  it('delete request to api/drivers to delete', done => {
+    const driver = new Driver({ email: 'test@test.com' });
+
+    driver.save()
+      .then(() => {
+        request(app)
+          .delete(`/api/drivers/${driver._id}`)
+          .end(() => {
+            Driver.findOne({ email: 'test@test' })
+              .then(driver => {
+                assert(driver === null)
+                done();
+              })
+          })
+
+      })
+  })
+
 })
